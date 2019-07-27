1a9dc501d5e7936bd412af845e03f20ef6d30b43
zhanghuang <970839007@qq.com>
2019-07-27 12:03:40 +0800

    First all

---
+++ src/cn/test/hellowoeld.java
@@ -0,0 +1,7 @@
+package cn.test;
+
+public class hellowoeld {
+    public static void main(String[] args) {
+        System.out.println("helloworld,hello git");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code hers
+        System.out.println("zhanghuang");
+    }
+}
